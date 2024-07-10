5b0a4c2a1fd04a7e38b65135e893da1f560483b1
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-07-09 22:52:16 -0500

    refactor (create_db.py): append to db instead of replace (woof!)

--- bin/create_db.py
+++ bin/create_db.py
@@ -18,7 +18,7 @@ df = pd.read_csv(csv_file, header=0)
 
 conn = sqlite3.connect(db_file)
 
-df.to_sql('cooling_centers', conn, if_exists='replace', index=False)
+df.to_sql('cooling_centers', conn, if_exists='append', index=False)
 
 print("DB created successfully! The first 5 rows are:\n")
 query = "SELECT * FROM cooling_centers LIMIT 5"
